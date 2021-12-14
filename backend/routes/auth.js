218fe85bdc9509b4d27b7789823bb3b32afeaf31
= <abhisingh30800@gmail.com>
2021-12-14 20:36:18 +0530

    Video 51 Completed

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 var jwt = require("jsonwebtoken");
+var fetchUser = require("../middleware/fetchUser");
 
 const JWT_SECRET = "Abhiisaprogramm$er";
 
-// Create a user using : POST "/api/auth/createuser". No login required
+// Route 1 : Create a user using : POST "/api/auth/createuser". No login required
 router.post(
   "/createuser",
   [
@@ -60,7 +61,7 @@ router.post(
   }
 );
 
-// Authenticate a user using : POST "/api/auth/login". No login required
+// Route 2 :  Authenticate a user using : POST "/api/auth/login". No login required
 router.post(
   "/login",
   [
@@ -104,4 +105,17 @@ router.post(
     }
   }
 );
+
+// Route 3 : Get loggedin User Details using : POST "/api/auth/getuser" . Login required
+router.post("/getuser", fetchUser, async (req, res) => {
+try {
+  const userId = req.user.id;
+  const user = await User.findById(userId).select("-password");
+  res.send(user);
+} catch (error) {
+  console.error(error.message);
+  res.status(500).send("Internal Server Error");
+}
+})
+
 module.exports = router;
