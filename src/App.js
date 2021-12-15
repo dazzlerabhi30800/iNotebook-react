97cd40aaa864e0695fc1f950e2c2c8e418e5ab40
= <abhisingh30800@gmail.com>
2021-12-15 12:44:52 +0530

    Video 56 Completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNotebook!</h1>
-    </div>
+    <>
+    <Router>
+
+    <Navbar />
+    {/* <About /> */}
+    
+      <Routes>
+        <Route exact path = "/about" element= {<About />} />
+        <Route exact path = "/" element= {<Home />} />
+      </Routes>
+    </Router>
+    </>
   );
 }
 
