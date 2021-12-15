3405eb66a72452e43cd4ad7c15f9a8ed3ed862ec
= <abhisingh30800@gmail.com>
2021-12-15 19:23:07 +0530

    Video 61 Completed

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -12,7 +12,52 @@ const NoteState = (props) => {
           "tag": "Coding",
           "date": "2021-12-15T05:46:31.110Z",
           "__v": 0
-        }
+        },
+        {
+          "_id": "61b981371aacfdeb91296b25",
+          "user": "61b8a47d829c181574aa94a9",
+          "title": "New Note",
+          "description": "Please code everyday",
+          "tag": "Coding",
+          "date": "2021-12-15T05:46:31.110Z",
+          "__v": 0
+        },
+        {
+          "_id": "61b981371aacfdeb91296b25",
+          "user": "61b8a47d829c181574aa94a9",
+          "title": "New Note",
+          "description": "Please code everyday",
+          "tag": "Coding",
+          "date": "2021-12-15T05:46:31.110Z",
+          "__v": 0
+        },
+        {
+          "_id": "61b981371aacfdeb91296b25",
+          "user": "61b8a47d829c181574aa94a9",
+          "title": "New Note",
+          "description": "Please code everyday",
+          "tag": "Coding",
+          "date": "2021-12-15T05:46:31.110Z",
+          "__v": 0
+        },
+        {
+          "_id": "61b981371aacfdeb91296b25",
+          "user": "61b8a47d829c181574aa94a9",
+          "title": "New Note",
+          "description": "Please code everyday",
+          "tag": "Coding",
+          "date": "2021-12-15T05:46:31.110Z",
+          "__v": 0
+        },
+        {
+          "_id": "61b981371aacfdeb91296b25",
+          "user": "61b8a47d829c181574aa94a9",
+          "title": "New Note",
+          "description": "Please code everyday",
+          "tag": "Coding",
+          "date": "2021-12-15T05:46:31.110Z",
+          "__v": 0
+        },
       ]
       const [notes, setNotes] = useState(notesInitial)
     return (
